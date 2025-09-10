7ed7c3bda9d9b6e476b9202016feeb9d5dca9d69
Silver [QloudNova Tech] <erhieyovwegreat7@gmail.com>
2025-09-10 11:24:49 +0100

    Update register.js render

--- Cboe/public_html/assets/js/register.js
+++ Cboe/public_html/assets/js/register.js
@@ -264,7 +264,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // Check if username or email already exists in db
-                const response = await fetch(`http://localhost:10000/users?username=${username}&email=${email}`);
+                const response = await fetch(`https://cboe.onrender.com/users?username=${username}&email=${email}`);
                 const existingUsers = await response.json();
 
                 const usernameExists = existingUsers.some(user => user.username === username);
@@ -285,7 +285,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     password // In real-world apps, never store raw passwords!
                 };
 
-                const postRes = await fetch(`http://localhost:10000/users`, {
+                const postRes = await fetch(`https://cboe.onrender.com/users`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
